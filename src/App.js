1e16ff7966fd116d13c113b1a21db536ffaa4c68
Sahil Sachdev <isahilsachdev00@gmail.com>
2024-04-11 15:46:39 +0530

    Initial UI with static json data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import JSONdata from './data.json';
 
 function App() {
+  const [data, setData] = useState(JSONdata);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className="input-container">
+        <div className="card-inner">
+          {<label>2 results found</label>}
+          <div className="container">
+            <div className="search-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#657789" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
+            </div>
+            <div className="input-box">
+              <input placeholder="Search by ID, address, name, item or pincode"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='results-container'>
+        {
+          data?.length > 0 ? (
+            data.map((item, id) => (
+              <div key={`${item.id}`} className="result-box">
+                <div className="result-box-header">
+                  <div>
+                    {item.name}
+                  </div>
+                  <div>
+                    {item.items.join(', ')}
+                  </div>
+                </div>
+                <div className="result-box-header">
+                  <div>
+                    {item.address}
+                  </div>
+                  <div>
+                    {item.pincode}
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>
+              No data found
+            </div>  
+          )
+        }
+      </div>
     </div>
   );
 }
